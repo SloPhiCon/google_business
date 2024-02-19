b1b485ba1b49fc7595b1a82e087bcb476ecbdf33
Sachin4B4 <139662439+Sachin4B4@users.noreply.github.com>
2024-02-19 08:49:51 -0800

    javascript-code.js

--- javascript-code.js
+++ javascript-code.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('googleSignInBtn').addEventListener('click', function() {
         // Redirect to Google authentication page
-        window.location.href = '/auth/google';
+        window.location.href = 'https://accounts.google.com/o/oauth2/v2/auth?client_id=431062006675-mohumg8orrinedqrv4s7a8c4bts0ampg.apps.googleusercontent.com&redirect_uri=https://sachin4b4.github.io/google_business/auth/callback&response_type=code&scope=openid%20https://www.googleapis.com/auth/business.manage';
     });
 });
