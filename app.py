0f7656ed2eac92095dd1d3de1a9c6074ff46d044
Sachin4B4 <139662439+Sachin4B4@users.noreply.github.com>
2024-10-07 15:45:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,66 +1,10 @@
-from flask import Flask, request, jsonify, redirect, url_for, session
-from onelogin.saml2.auth import OneLogin_Saml2_Auth
+from flask import Flask, request, jsonify
 
 app = Flask(_name_)
 
 @app.route('/')
 def say_hi():
     return 'Hi! This is the addition service.'
-
-app.secret_key = os.urandom(24)  # Replace this with a secure key in production
-
-# Configuration for SAML
-SAML_PATH = os.path.join(os.getcwd(), 'saml')
-
-
-def init_saml_auth(req):
-    auth = OneLogin_Saml2_Auth(req, custom_base_path=SAML_PATH)
-    return auth
-
-def prepare_flask_request(request):
-    url_data = request.url.split('?')
-    return {
-        'https': 'on' if request.scheme == 'https' else 'off',
-        'http_host': request.host,
-        'script_name': request.path,
-        'server_port': request.host.split(':')[1] if ':' in request.host else '80',
-        'get_data': request.args.copy(),
-        'post_data': request.form.copy(),
-    }
-
-
-@app.route('/saml/login')
-def login():
-    req = prepare_flask_request(request)
-    auth = init_saml_auth(req)
-    return redirect(auth.login())
-
-'''
-@app.route('/saml/logout')
-def logout():
-    req = prepare_flask_request(request)
-    auth = init_saml_auth(req)
-    name_id = session['samlNameId'] if 'samlNameId' in session else None
-    session.clear()
-    return redirect(auth.logout(name_id=name_id))
-'''
-
-@app.route('/saml/callback', methods=['POST'])
-def login_callback():
-    req = prepare_flask_request(request)
-    auth = init_saml_auth(req)
-    auth.process_response()
-    errors = auth.get_errors()
-
-    if not errors:
-        session['samlUserdata'] = auth.get_attributes()
-        session['samlNameId'] = auth.get_nameid()
-        name_ = session['samlUserdata']['http://schemas.microsoft.com/identity/claims/displayname']
-        #return redirect(url_for('index'))
-        return f"{name_} User authenticated!"
-    else:
-        return f"Error in SAML Authentication: {errors}", 500
-[15:38, 07/10/2024] Sachin Bhusnurmath: from f
 
 @app.route('/add', methods=['POST'])
 def add_numbers():
