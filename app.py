3d5d0a46c0e8dba3f283abca4fca83548228bd83
Sachin4B4 <139662439+Sachin4B4@users.noreply.github.com>
2024-10-07 13:25:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,17 +10,4 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
-@app.route('/add', methods=['POST'])
-def add():
-    data = request.get_json()
-    result = data['a'] + data['b']
-    return jsonify(result=result)
 
-@app.route('/multiply', methods=['POST'])
-def multiply():
-    data = request.get_json()
-    result = data['a'] * data['b']
-    return jsonify(result=result)
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
