32da52f367c381f694a3b49251fc87edce290f21
Sachin4B4 <139662439+Sachin4B4@users.noreply.github.com>
2024-10-07 13:07:14 +0530

    Add files via upload

---
+++ from flask import Flask, request, jsonif.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/add', methods=['POST'])
+def add():
+    data = request.get_json()
+    result = data['a'] + data['b']
+    return jsonify(result=result)
+
+@app.route('/multiply', methods=['POST'])
+def multiply():
+    data = request.get_json()
+    result = data['a'] * data['b']
+    return jsonify(result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
