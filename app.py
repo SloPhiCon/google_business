e841ffdc5705c7e25787b10f01a69fe912261b59
Sachin4B4 <139662439+Sachin4B4@users.noreply.github.com>
2024-10-07 15:08:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,28 @@ app = Flask(__name__)
 def say_hi():
     return 'Hi! This is the addition service.'
 
+@app.route('/saml/login', methods=['GET'])
+def saml_login():
+    logging.info('saml_login function processed a request.')
+    req_data = prepare_request()
+    auth = init_saml_auth(req_data)
+    return redirect(auth.login())
+
+@app.route('/saml/callback', methods=['POST'])
+def saml_callback():
+    logging.info('saml_callback function processed a request.')
+    req_data = prepare_request()
+    auth = init_saml_auth(req_data)
+    auth.process_response()
+    errors = auth.get_errors()
+
+    if not errors:
+        user_data = auth.get_attributes()
+        name_ = user_data.get('http://schemas.microsoft.com/identity/claims/displayname', ['User'])[0]
+        return jsonify({"message": f"{name_} User authenticated!"})
+    else:
+        return jsonify({"error": "Error in SAML Authentication", "details": errors}), 500
+
 @app.route('/add', methods=['POST'])
 def add_numbers():
     # Get JSON data from the request
