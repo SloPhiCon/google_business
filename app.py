0be5f3eda9cc1b52366ea70d35528205f91ef105
Sachin4B4 <139662439+Sachin4B4@users.noreply.github.com>
2024-10-07 13:17:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "Hello, Azure! Your Flask app is deployed successfully."
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
 @app.route('/add', methods=['POST'])
 def add():
     data = request.get_json()
